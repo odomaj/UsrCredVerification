017e283ea253a4d417c3dde8a12055aa6114e809
Megan <edwardmm@Clarkson.edu>
2024-03-12 11:37:22 -0400

    Added front end webstite + encrypts the entered and password

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request, jsonify
+import CredentialHandler
+
 
 app = Flask(__name__)
 
-@app.get("/")
-def indes_get():
-    return render_template("base.html")
+enteredCreds = CredentialHandler.CredentialHandler()
+    
+@app.route("/", methods = ["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+        #getting input with email = email in HTML form
+        email = request.form.get("email")
+        #getting input with password = password in HTML form
+        password = request.form.get("password")
 
-@app.post("/predict")
-def predict():
-    return('New Message')
+        #hashes the email and password
+        email = enteredCreds.encrypt(email)
+        password = enteredCreds.encrypt(password)
+
+        return "Your credentials are: " + email + " " + password
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
